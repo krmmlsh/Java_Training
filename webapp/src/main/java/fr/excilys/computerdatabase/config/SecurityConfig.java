a13c43fcf52e32f9ed2cf3641dad31ec09d209f8
azertyuiop <excilys@localhost.localdomain>
2017-12-12 11:45:02 +0100

    dummy commit

--- webapp/src/main/java/fr/excilys/computerdatabase/config/SecurityConfig.java
+++ webapp/src/main/java/fr/excilys/computerdatabase/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	UserServices userService;
 	
 
+	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService);
